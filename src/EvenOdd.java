ef0e126d6015008b3b71eaa8b73738fa0d3609c0
Sanjanadas45 <sanjana45das@gmail.com>
2025-06-11 21:38:19 +0530

    init commit

--- src/EvenOdd.java
+++
@@ -1,9 +0,0 @@
-public class EvenOdd {
-    public static void main(String[] args) {
-        int n = 67;
-        System.out.println(isOdd(n));
-    }
-    private static boolean isOdd(int n){
-        return (n & 1) == 1;
-    }
-}
